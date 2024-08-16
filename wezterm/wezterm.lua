c43ef86e232886194893c0490d153afe89ef87c5
Sebastian Villegas <sebaena@gmail.com>
2024-08-16 20:24:45 +0300

    updated wezterm theme, added logic to tab coloring

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,8 +7,30 @@ local config = wezterm.config_builder()
 -- set ubuntu as default when opening. Comment this if using linux
 config.default_domain = 'WSL:Ubuntu-22.04'
 
+-- set powershell as default shell in windows
+config.default_prog = { 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe' }
+
 -- set color scheme
-config.color_scheme = 'TweakedPandora'
+-- my usuals are (come by default)= 'nightfox', 'TweakedPandora' , 'Night Owl (Gogh)', 'Kanagawa (Gogh)', 'kanagawabones'
+theme = 'nightfox'
+
+config.color_scheme = theme
+
+-- set tab passive and active background colors based on theme
+themeActiveBackground = '#5ae8c2' -- Use the same active color for now
+if theme == 'TweakedPandora' then
+  themePassiveBackground = '#141e43'
+elseif theme == 'Night Owl (Gogh)' then
+  themePassiveBackground = '#011627'
+elseif theme == 'nightfox' then
+  themePassiveBackground = '#192330'
+elseif theme == 'Kanagawa (Gogh)' then
+  themePassiveBackground = '#1F1F28'
+elseif theme == 'kanagawabones' then
+  themePassiveBackground = '#1f1f28'
+else
+  themePassiveBackground = 'black'
+end
 
 -- set font
 config.font = wezterm.font('Iosevka Nerd Font', { weight = 'Medium' })
@@ -29,17 +51,18 @@ config.audible_bell = "Disabled"
 
 -- Format tabs
 config.tab_bar_at_bottom = false
-config.use_fancy_tab_bar = false
+-- config.use_fancy_tab_bar = false
 
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
     local edge_background = '#141e43'
-    local background = '#141e43'
+    -- local background = '#141e43' -- este hay que cambiarlo
+    local background = themePassiveBackground -- este hay que cambiarlo
     local foreground = '#808080'
 
     if tab.is_active then
-      background = '#5ae8c2'
+      background = themeActiveBackground
       foreground = 'black'
     elseif hover then
       background = '#3b3052'
@@ -55,17 +78,19 @@ wezterm.on(
     return {
       { Background = { Color = edge_background } },
       { Foreground = { Color = edge_foreground } },
-      { Text = " " },
+      -- { Text = " " },
       { Background = { Color = background } },
       { Foreground = { Color = foreground } },
       { Text = " " .. (tab.tab_index + 1) .. ": " .. title .. " " },
       { Background = { Color = edge_background } },
       { Foreground = { Color = edge_foreground } },
-      { Text = "" },
+      -- { Text = "" },
       -- { Text = SOLID_RIGHT_ARROW },
     }
   end
 )
+
+
 
 -- return the configuration to wezterm
 return config
