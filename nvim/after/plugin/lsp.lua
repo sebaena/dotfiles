972ebd26f84afc7276d5baf8d7133bc100135c60
sebastian villegas <sebaena@gmail.com>
2024-08-25 12:08:13 +0300

    changed nvim lsp configuration

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -40,17 +40,18 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymnp.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymnp.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leaeer>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  -- vim.keymnp.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  -- vim.keymnp.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+  -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  -- vim.keymap.set("n", "<leaeer>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 
 end)
 
 require('lspconfig').clangd.setup { }
+require 'lspconfig'.pyright.setup {}
 
 lsp.setup()
 
